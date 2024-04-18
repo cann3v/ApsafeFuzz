00703ccd8a7c4dcb44e7e335328c943ad4c7c09a
Roman Kornilov <kornilovroman123@gmail.com>
2024-04-18 12:51:29 +0500

    [#1] Collect credentials for ansible

---
+++ Models/ClusterConfigurationModel.cs
@@ -0,0 +1,8 @@
+namespace ApSafeFuzz.Models;
+
+public class ClusterConfigurationModel
+{
+    public string? IpAddress { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
