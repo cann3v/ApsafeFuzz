6c61672d5af632f3c9c1b7e4e24697123057c075
Roman Kornilov <kornilovroman123@gmail.com>
2024-04-23 01:30:48 +0500

    [#4] Add database migration for cluster configuration data

--- Models/ClusterConfigurationModel.cs
+++ Models/ClusterConfigurationModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ApSafeFuzz.Models;
 
 public class ClusterConfigurationModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
     public string? IpAddress { get; set; }
     public string? Username { get; set; }
     public string? Password { get; set; }
