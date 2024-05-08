457c2e57c5f3b86ab69ff7d9b6f589f811a8e5fc
Roman Kornilov <kornilovroman123@gmail.com>
2024-05-09 00:42:53 +0500

    Delete build files from disk

--- Utilities/FileHelper.cs
+++ Utilities/FileHelper.cs
@@ -16,4 +16,33 @@ public static class FileHelper
 
         return fileName.Substring(extensionIndex);
     }
+
+    /// <summary>
+    /// Метод для удаления файла.
+    /// </summary>
+    /// <param name="filePath">Путь к файлу, который необходимо удалить.</param>
+    /// <returns>
+    /// 0 - если файл успешно удален.
+    /// 1 - если файл не существует.
+    /// 2 - если произошла другая ошибка при удалении файла.
+    /// </returns>
+    public static int DeleteFile(string filePath)
+    {
+        try
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        catch (Exception ex)
+        {
+            return 2;
+        }
+    }
 }
